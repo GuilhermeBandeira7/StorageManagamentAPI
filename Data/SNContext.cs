a1fb792710b742679633b52b2f5c093190f496f7
Guilherme Bandeira <83375623+GuilhermeBandeira7@users.noreply.github.com>
2024-04-29 15:16:44 -0300

    Security Update

--- Data/SNContext.cs
+++ Data/SNContext.cs
@@ -15,13 +15,7 @@ namespace SNGeneratorAPI.Data
         public DbSet<Usuario> Usuario { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            //optionsBuilder.UseSqlServer(@"Server=172.233.25.218;Database=SNGenerator;User id=sa;Password=Senha@tmhub;Trusted_Connection=False; TrustServerCertificate=True", builder =>           
-            //{
-            //    builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
-            //});
-            //base.OnConfiguring(optionsBuilder);
-
+        {            
             optionsBuilder.UseSqlServer(@"Server=localhost;Database=SNGenerator;User id=sa;Password=Senha@mtw;Trusted_Connection=False; TrustServerCertificate=True", builder =>
             {
                 builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
